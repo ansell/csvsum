53b377dc9d8bb41eade2679c75f5fdaec7927590
Peter Ansell <p_ansell@yahoo.com>
2016-02-01 22:27:37 -0500

    Add more tests

--- src/test/java/com/github/ansell/csvsum/CSVUtilTest.java
+++ src/test/java/com/github/ansell/csvsum/CSVUtilTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Rule;
@@ -36,6 +37,122 @@ public class CSVUtilTest {
 		thrown.expect(RuntimeException.class);
 		thrown.expectMessage("CSV file did not contain a valid header line");
 		CSVUtil.streamCSV(new StringReader(""), h -> headers.addAll(h), (h, l) -> l, l -> lines.add(l));
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVHeaderOnlySingleColumn() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(new StringReader("Test1"), h -> headers.addAll(h), (h, l) -> l, l -> lines.add(l));
+		assertEquals(1, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertEquals(0, lines.size());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVHeaderOnlyMultipleColumns() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(new StringReader("Test1, Another2, Else3"), h -> headers.addAll(h), (h, l) -> l,
+				l -> lines.add(l));
+		assertEquals(3, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertTrue(headers.contains("Another2"));
+		assertTrue(headers.contains("Else3"));
+		assertEquals(0, lines.size());
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVSingleRowSingleColumn() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(new StringReader("Test1\nAnswer1"), h -> headers.addAll(h), (h, l) -> l, l -> lines.add(l));
+		assertEquals(1, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertEquals(1, lines.size());
+		assertTrue(lines.contains(Arrays.asList("Answer1")));
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVSingleRowMultipleColumns() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(new StringReader("Test1, Another2, Else3\nAnswer1, Alternative2, Attempt3"),
+				h -> headers.addAll(h), (h, l) -> l, l -> lines.add(l));
+		assertEquals(3, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertTrue(headers.contains("Another2"));
+		assertTrue(headers.contains("Else3"));
+		assertEquals(1, lines.size());
+		assertTrue(lines.contains(Arrays.asList("Answer1", "Alternative2", "Attempt3")));
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVMultipleRowsSingleColumn() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(new StringReader("Test1\nAnswer1\nAnswer2\nAnswer3"), h -> headers.addAll(h), (h, l) -> l,
+				l -> lines.add(l));
+		assertEquals(1, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertEquals(3, lines.size());
+		assertTrue(lines.contains(Arrays.asList("Answer1")));
+		assertTrue(lines.contains(Arrays.asList("Answer2")));
+		assertTrue(lines.contains(Arrays.asList("Answer3")));
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csvsum.CSVUtil#streamCSV(java.io.Reader, java.util.function.Consumer, java.util.function.BiFunction, java.util.function.Consumer)}
+	 * .
+	 */
+	@Test
+	public final void testStreamCSVMultipleRowsMultipleColumns() throws Exception {
+		List<String> headers = new ArrayList<>();
+		List<List<String>> lines = new ArrayList<>();
+
+		CSVUtil.streamCSV(
+				new StringReader(
+						"Test1, Another2, Else3\nAnswer1, Alternative2, Attempt3\nAnswer4, Alternative5, Attempt6\nAnswer7, Alternative8, Attempt9"),
+				h -> headers.addAll(h), (h, l) -> l, l -> lines.add(l));
+		assertEquals(3, headers.size());
+		assertTrue(headers.contains("Test1"));
+		assertTrue(headers.contains("Another2"));
+		assertTrue(headers.contains("Else3"));
+		assertEquals(3, lines.size());
+		assertTrue(lines.contains(Arrays.asList("Answer1", "Alternative2", "Attempt3")));
+		assertTrue(lines.contains(Arrays.asList("Answer4", "Alternative5", "Attempt6")));
+		assertTrue(lines.contains(Arrays.asList("Answer7", "Alternative8", "Attempt9")));
 	}
 
 }
