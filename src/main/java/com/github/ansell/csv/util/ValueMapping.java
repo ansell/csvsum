bc2a6e3af0271d83357084b09b6f2e5beb72dced
Peter Ansell <p_ansell@yahoo.com>
2016-02-18 17:08:04 -0500

    reengineer ValueMapping to have final fields and equals/hashCode

--- src/main/java/com/github/ansell/csv/util/ValueMapping.java
+++ src/main/java/com/github/ansell/csv/util/ValueMapping.java
@@ -48,26 +48,22 @@ import javax.script.ScriptException;
  */
 public class ValueMapping {
 
+	public enum ValueMappingLanguage {
+		DEFAULT, JAVASCRIPT, GROOVY, LUA, ACCESS
+	}
+
 	/**
 	 * The default mapping if none is specified in the mapping file.
 	 */
 	protected static final String DEFAULT_MAPPING = "inputValue";
 
-	public enum ValueMappingLanguage {
-		DEFAULT, JAVASCRIPT, GROOVY, LUA
-	}
-
-	private ValueMappingLanguage language;
-	private String input;
-	private String output;
-	private String mapping = DEFAULT_MAPPING;
 	public static final String LANGUAGE = "Language";
+
 	public static final String OLD_FIELD = "OldField";
+
 	public static final String NEW_FIELD = "NewField";
 	public static final String MAPPING = "Mapping";
-
 	private static final ScriptEngineManager SCRIPT_MANAGER = new ScriptEngineManager();
-
 	private static final boolean DEBUG = false;
 
 	static {
@@ -78,39 +74,194 @@ public class ValueMapping {
 		}
 	}
 
-	private ScriptEngine scriptEngine;
-	private CompiledScript compiledScript;
+	public static List<ValueMapping> extractMappings(Reader input) throws IOException {
+		List<ValueMapping> result = new ArrayList<>();
+
+		List<String> headers = new ArrayList<>();
+
+		CSVUtil.streamCSV(input, h -> headers.addAll(h), (h, l) -> {
+			return newMapping(l.get(h.indexOf(LANGUAGE)), l.get(h.indexOf(OLD_FIELD)), l.get(h.indexOf(NEW_FIELD)),
+					l.get(h.indexOf(MAPPING)));
+		} , l -> result.add(l));
+
+		return result;
+	}
+
+	public static List<String> mapLine(List<String> inputHeaders, List<String> outputHeaders, List<String> line,
+			List<ValueMapping> map) {
+
+		if (outputHeaders.size() != map.size()) {
+			throw new IllegalArgumentException("The number of mappings must match the number of output headers");
+		}
+
+		Map<String, String> outputValues = new ConcurrentHashMap<>();
+		List<String> result = new ArrayList<>();
+
+		// Note, empirically, it seems about 50% faster with a limited number of
+		// cores to do a serial mapping, not a parallel mapping
+		// map.parallelStream().forEach(nextMapping -> {
+		// for (ValueMapping nextMapping : map) {
+		map.forEach(nextMapping -> {
+			String mappedValue = nextMapping.apply(inputHeaders, line);
+			outputValues.put(nextMapping.getOutputField(), mappedValue);
+		});
+
+		outputHeaders.forEach(nextOutput -> result.add(outputValues.get(nextOutput)));
+
+		return result;
+	}
+
+	public static final ValueMapping newMapping(String language, String input, String output, String mapping) {
+		ValueMappingLanguage nextLanguage;
+		try {
+			nextLanguage = ValueMappingLanguage.valueOf(language.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			nextLanguage = ValueMappingLanguage.DEFAULT;
+		}
+
+		if (nextLanguage == null) {
+			throw new IllegalArgumentException("No mapping language found for: " + language);
+		}
+
+		String nextMapping;
+		// By default empty mappings do not change the input, and are
+		// efficiently dealt with as such
+		if (!mapping.isEmpty()) {
+			nextMapping = mapping;
+		} else {
+			nextMapping = DEFAULT_MAPPING;
+		}
+
+		ValueMapping result = new ValueMapping(nextLanguage, input, output, nextMapping);
+
+		result.init();
+
+		return result;
+	}
+
+	private final ValueMappingLanguage language;
+
+	private final String input;
+
+	private final String output;
+
+	private final String mapping;
+
+	private transient ScriptEngine scriptEngine;
+	private transient CompiledScript compiledScript;
 
 	/**
 	 * All creation of ValueMapping objects must be done through the
 	 * {@link #newMapping(String, String, String, String)} method.
 	 */
-	private ValueMapping() {
+	private ValueMapping(ValueMappingLanguage language, String input, String output, String mapping) {
+		this.language = language;
+		this.input = input;
+		this.output = output;
+		this.mapping = mapping;
 	}
 
-	public static final ValueMapping newMapping(String language, String input, String output, String mapping) {
-		ValueMapping result = new ValueMapping();
+	private String apply(List<String> inputHeaders, List<String> line) {
+		String nextInputValue = line.get(inputHeaders.indexOf(getInputField()));
 
-		try {
-			result.language = ValueMappingLanguage.valueOf(language.toUpperCase());
-		} catch (IllegalArgumentException e) {
-			result.language = ValueMappingLanguage.DEFAULT;
+		// Short circuit if the mapping is the default mapping
+		if (this.mapping.equalsIgnoreCase(DEFAULT_MAPPING) || this.language == ValueMappingLanguage.DEFAULT) {
+			return nextInputValue;
 		}
 
-		if (result.language == null) {
-			throw new IllegalArgumentException("No mapping language found for: " + language);
+		if (this.language == ValueMappingLanguage.JAVASCRIPT || this.language == ValueMappingLanguage.GROOVY
+				|| this.language == ValueMappingLanguage.LUA) {
+
+			try {
+				if (scriptEngine instanceof Invocable) {
+					// evaluate script code and access the variable that results
+					// from the mapping
+					return (String) ((Invocable) scriptEngine).invokeFunction("mapFunction", inputHeaders,
+							this.getInputField(), nextInputValue, this.getOutputField(), line);
+				} else if (compiledScript != null) {
+					Bindings bindings = scriptEngine.createBindings();
+					// inputHeaders, inputField, inputValue, outputField, line
+					bindings.put("inputHeaders", inputHeaders);
+					bindings.put("inputField", this.getInputField());
+					bindings.put("inputValue", nextInputValue);
+					bindings.put("outputField", this.getOutputField());
+					bindings.put("line", line);
+					return (String) compiledScript.eval(bindings);
+				} else {
+					throw new UnsupportedOperationException(
+							"Cannot handle results from ScriptEngine.eval that are not Invocable or CompiledScript");
+				}
+			} catch (ScriptException | NoSuchMethodException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			throw new UnsupportedOperationException("Mapping language not supported: " + this.language);
 		}
+	}
 
-		result.input = input;
-		result.output = output;
-		// By default empty mappings do not change the input, and are
-		// efficiently dealt with as such
-		if (!mapping.isEmpty()) {
-			result.mapping = mapping;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
 		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ValueMapping)) {
+			return false;
+		}
+		ValueMapping other = (ValueMapping) obj;
+		if (input == null) {
+			if (other.input != null) {
+				return false;
+			}
+		} else if (!input.equals(other.input)) {
+			return false;
+		}
+		if (language != other.language) {
+			return false;
+		}
+		if (mapping == null) {
+			if (other.mapping != null) {
+				return false;
+			}
+		} else if (!mapping.equals(other.mapping)) {
+			return false;
+		}
+		if (output == null) {
+			if (other.output != null) {
+				return false;
+			}
+		} else if (!output.equals(other.output)) {
+			return false;
+		}
+		return true;
+	}
 
-		result.init();
+	public String getInputField() {
+		return this.input;
+	}
 
+	public ValueMappingLanguage getLanguage() {
+		return this.language;
+	}
+
+	public String getMapping() {
+		return this.mapping;
+	}
+
+	public String getOutputField() {
+		return this.output;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((input == null) ? 0 : input.hashCode());
+		result = prime * result + ((language == null) ? 0 : language.hashCode());
+		result = prime * result + ((mapping == null) ? 0 : mapping.hashCode());
+		result = prime * result + ((output == null) ? 0 : output.hashCode());
 		return result;
 	}
 
@@ -152,97 +303,5 @@ public class ValueMapping {
 		} else {
 			throw new UnsupportedOperationException("Mapping language not supported: " + this.language);
 		}
-	}
-
-	public ValueMappingLanguage getLanguage() {
-		return this.language;
-	}
-
-	public String getInputField() {
-		return this.input;
-	}
-
-	public String getOutputField() {
-		return this.output;
-	}
-
-	public String getMapping() {
-		return this.mapping;
-	}
-
-	public static List<String> mapLine(List<String> inputHeaders, List<String> outputHeaders, List<String> line,
-			List<ValueMapping> map) {
-
-		if (outputHeaders.size() != map.size()) {
-			throw new IllegalArgumentException("The number of mappings must match the number of output headers");
-		}
-
-		Map<String, String> outputValues = new ConcurrentHashMap<>();
-		List<String> result = new ArrayList<>();
-
-		// Note, empirically, it seems about 50% faster with a limited number of
-		// cores to do a serial mapping, not a parallel mapping
-		// map.parallelStream().forEach(nextMapping -> {
-		// for (ValueMapping nextMapping : map) {
-		map.forEach(nextMapping -> {
-			String mappedValue = nextMapping.apply(inputHeaders, line);
-			outputValues.put(nextMapping.getOutputField(), mappedValue);
-		});
-
-		outputHeaders.forEach(nextOutput -> result.add(outputValues.get(nextOutput)));
-
-		return result;
-	}
-
-	private String apply(List<String> inputHeaders, List<String> line) {
-		String nextInputValue = line.get(inputHeaders.indexOf(getInputField()));
-
-		// Short circuit if the mapping is the default mapping
-		if (this.mapping.equalsIgnoreCase(DEFAULT_MAPPING) || this.language == ValueMappingLanguage.DEFAULT) {
-			return nextInputValue;
-		}
-
-		if (this.language == ValueMappingLanguage.JAVASCRIPT || this.language == ValueMappingLanguage.GROOVY
-				|| this.language == ValueMappingLanguage.LUA) {
-
-			try {
-				if (scriptEngine instanceof Invocable) {
-					// evaluate script code and access the variable that results
-					// from the mapping
-					return (String) ((Invocable) scriptEngine).invokeFunction("mapFunction", inputHeaders,
-							this.getInputField(), nextInputValue, this.getOutputField(), line);
-				} else if (compiledScript != null) {
-					Bindings bindings = scriptEngine.createBindings();
-					// inputHeaders, inputField, inputValue, outputField, line
-					bindings.put("inputHeaders", inputHeaders);
-					bindings.put("inputField", this.getInputField());
-					bindings.put("inputValue", nextInputValue);
-					bindings.put("outputField", this.getOutputField());
-					bindings.put("line", line);
-					return (String) compiledScript.eval(bindings);
-				} else {
-					throw new UnsupportedOperationException(
-							"Cannot handle results from ScriptEngine.eval that are not Invocable or CompiledScript");
-				}
-			} catch (ScriptException | NoSuchMethodException e) {
-				throw new RuntimeException(e);
-			}
-		} else {
-			throw new UnsupportedOperationException("Mapping language not supported: " + this.language);
-		}
-	}
-
-	public static List<ValueMapping> extractMappings(Reader input) throws IOException {
-		List<ValueMapping> result = new ArrayList<>();
-	
-		List<String> headers = new ArrayList<>();
-	
-		CSVUtil.streamCSV(input, h -> headers.addAll(h), (h, l) -> {
-			return newMapping(l.get(h.indexOf(LANGUAGE)),
-					l.get(h.indexOf(OLD_FIELD)), l.get(h.indexOf(NEW_FIELD)),
-					l.get(h.indexOf(MAPPING)));
-		} , l -> result.add(l));
-	
-		return result;
 	}
 }
