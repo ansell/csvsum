03b2b6496b803459759b64c3ec33532541c1cac7
Peter Ansell <p_ansell@yahoo.com>
2016-02-18 23:06:23 -0500

    Add more ValueMapping tests

--- src/test/java/com/github/ansell/csv/util/ValueMappingTest.java
+++ src/test/java/com/github/ansell/csv/util/ValueMappingTest.java
@@ -2,6 +2,10 @@ package com.github.ansell.csv.util;
 
 import static org.junit.Assert.*;
 
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,13 +17,15 @@ public class ValueMappingTest {
 	private ValueMapping testDefaultMapping;
 	private ValueMapping testDefaultMapping2;
 	private ValueMapping testJavascriptMapping;
+	private ValueMapping testDefaultMapping3;
 
 	@Before
 	public void setUp() throws Exception {
 		testDefaultMapping = ValueMapping.newMapping("Default", "anInput", "anotherField", "");
 		testDefaultMapping2 = ValueMapping.newMapping("Default", "anInput", "anotherField", "inputValue");
+		testDefaultMapping3 = ValueMapping.newMapping("Default", "anInput3", "anotherField3", "inputValue");
 		testJavascriptMapping = ValueMapping.newMapping("Javascript", "aDifferentInput", "aDifferentField",
-				"return inputValue;");
+				"return inputValue.substring(0, 1);");
 	}
 
 	@After
@@ -33,13 +39,58 @@ public class ValueMappingTest {
 	}
 
 	@Test
-	public final void testExtractMappings() {
-		fail("Not yet implemented"); // TODO
+	public final void testExtractMappingsDefault() throws Exception {
+		List<ValueMapping> extractMappings = ValueMapping
+				.extractMappings(new StringReader("OldField,NewField,Language,Mapping\ninputField,outputField,,\n"));
+
+		assertEquals(1, extractMappings.size());
+		assertEquals("inputField", extractMappings.get(0).getInputField());
+		assertEquals("outputField", extractMappings.get(0).getOutputField());
+		assertEquals(ValueMappingLanguage.DEFAULT, extractMappings.get(0).getLanguage());
+		assertEquals(ValueMapping.DEFAULT_MAPPING, extractMappings.get(0).getMapping());
+	}
+
+	@Test
+	public final void testExtractMappingsJavascript() throws Exception {
+		List<ValueMapping> extractMappings = ValueMapping.extractMappings(new StringReader(
+				"OldField,NewField,Language,Mapping\ninputField2,outputField2,Javascript,\"return inputValue.trim();\"\n"));
+
+		assertEquals(1, extractMappings.size());
+		assertEquals("inputField2", extractMappings.get(0).getInputField());
+		assertEquals("outputField2", extractMappings.get(0).getOutputField());
+		assertEquals(ValueMappingLanguage.JAVASCRIPT, extractMappings.get(0).getLanguage());
+		assertEquals("return inputValue.trim();", extractMappings.get(0).getMapping());
+	}
+
+	@Test
+	public final void testExtractMappingsMultiple() throws Exception {
+		List<ValueMapping> extractMappings = ValueMapping.extractMappings(new StringReader(
+				"OldField,NewField,Language,Mapping\ninputField2,outputField2,Javascript,\"return inputValue.trim();\"\ninputField,outputField,,\n"));
+
+		assertEquals(2, extractMappings.size());
+
+		assertEquals("inputField2", extractMappings.get(0).getInputField());
+		assertEquals("outputField2", extractMappings.get(0).getOutputField());
+		assertEquals(ValueMappingLanguage.JAVASCRIPT, extractMappings.get(0).getLanguage());
+		assertEquals("return inputValue.trim();", extractMappings.get(0).getMapping());
+
+		assertEquals("inputField", extractMappings.get(1).getInputField());
+		assertEquals("outputField", extractMappings.get(1).getOutputField());
+		assertEquals(ValueMappingLanguage.DEFAULT, extractMappings.get(1).getLanguage());
+		assertEquals(ValueMapping.DEFAULT_MAPPING, extractMappings.get(1).getMapping());
 	}
 
 	@Test
 	public final void testMapLine() {
-		fail("Not yet implemented"); // TODO
+		List<String> mapLine = ValueMapping.mapLine(Arrays.asList("anInput", "anInput3", "aDifferentInput"),
+				Arrays.asList("anotherField", "anotherField3", "aDifferentField"),
+				Arrays.asList("testValue1", "testValue2", "xyzabc"),
+				Arrays.asList(testDefaultMapping, testDefaultMapping3, testJavascriptMapping));
+
+		assertEquals(3, mapLine.size());
+		assertEquals("testValue1", mapLine.get(0));
+		assertEquals("testValue2", mapLine.get(1));
+		assertEquals("x", mapLine.get(2));
 	}
 
 	@Test
@@ -64,7 +115,7 @@ public class ValueMappingTest {
 	public final void testGetMapping() {
 		assertEquals(ValueMapping.DEFAULT_MAPPING, testDefaultMapping.getMapping());
 		assertEquals(ValueMapping.DEFAULT_MAPPING, testDefaultMapping2.getMapping());
-		assertEquals("return inputValue;", testJavascriptMapping.getMapping());
+		assertEquals("return inputValue.substring(0, 1);", testJavascriptMapping.getMapping());
 	}
 
 	@Test
