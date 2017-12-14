8390709d74a88aa534bc7ed19fe31095625331a2
Peter Ansell <p_ansell@yahoo.com>
2017-12-15 09:59:29 +1100

    issue #27 : Add test for more than one entry
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/test/java/com/github/ansell/csv/sum/JSONSummariserTest.java
+++ src/test/java/com/github/ansell/csv/sum/JSONSummariserTest.java
@@ -116,8 +116,25 @@ public class JSONSummariserTest {
 		Path testFile = tempDir.newFile("test-array-single-entry.json").toPath();
 		Files.copy(this.getClass().getResourceAsStream("/com/github/ansell/csvsum/test-array-single-entry.json"),
 				testFile, StandardCopyOption.REPLACE_EXISTING);
-		Path testFields = tempDir.newFile("test-array-single-entry.csv").toPath();
-		Files.copy(this.getClass().getResourceAsStream("/com/github/ansell/csvsum/test-array-single-entry.csv"),
+		Path testFields = tempDir.newFile("test-array-json.csv").toPath();
+		Files.copy(this.getClass().getResourceAsStream("/com/github/ansell/csvsum/test-array-json.csv"),
+				testFields, StandardCopyOption.REPLACE_EXISTING);
+
+		JSONSummariser.main("--input", testFile.toAbsolutePath().toString(), "--fields",
+				testFields.toAbsolutePath().toString(), "--base-path", "/base");
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.csv.sum.JSONSummariser#main(java.lang.String[])}.
+	 */
+	@Test
+	public final void testMainArrayMultipleEntries() throws Exception {
+		Path testFile = tempDir.newFile("test-array-multiple-entries.json").toPath();
+		Files.copy(this.getClass().getResourceAsStream("/com/github/ansell/csvsum/test-array-multiple-entries.json"),
+				testFile, StandardCopyOption.REPLACE_EXISTING);
+		Path testFields = tempDir.newFile("test-array-json.csv").toPath();
+		Files.copy(this.getClass().getResourceAsStream("/com/github/ansell/csvsum/test-array-json.csv"),
 				testFields, StandardCopyOption.REPLACE_EXISTING);
 
 		JSONSummariser.main("--input", testFile.toAbsolutePath().toString(), "--fields",
